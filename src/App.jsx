dca78b5a3845155da5e94ac3defdad2eb9765cc4
Dariusz Lipinski <daro180@icloud.com>
2024-10-07 09:35:57 +0200

    basic-constructor

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <header id='top'><h1>Horse Racing</h1></header>
+      <section id='track'></section>
+    </>
+  )
+}
+
+export default App
